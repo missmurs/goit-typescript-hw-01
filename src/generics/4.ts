23dd8a36c7f8ebb4d32667d74f2ee33aefbe99d2
missmurs <131698859+missmurs@users.noreply.github.com>
2024-09-18 18:47:26 +0300

    Update task 4

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -1,1 +1,12 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
 
+function createOrUpdateUser(initialValues: Partial<User>) {}
+
+createOrUpdateUser({ email: "user@mail.com", password: "password123" });
+
+export {};
